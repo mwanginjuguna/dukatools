6c7b9552eb6767e08b54448a9b5206bf4543482c
kanothe <francis.kanothe@gmail.com>
2024-11-18 01:39:10 +0300

    Add inventory and inventory item model customizations.

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -4,8 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Inventory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::creating(function (Inventory $inventory) {
+            do {
+                // random ref
+                $ref = 'IVT' . substr(
+                        str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'),
+                        0, 8);
+
+            } while(Inventory::where('reference', $ref)->exists());
+
+            $inventory->reference = $ref;
+        });
+    }
+
+
+    public function inventoryItems(): HasMany
+    {
+        return $this->hasMany(InventoryItem::class);
+    }
+
+    public function vendor(): BelongsTo
+    {
+        return $this->belongsTo(Vendor::class);
+    }
+
+    public function business(): BelongsTo
+    {
+        return $this->belongsTo(Business::class);
+    }
 }
--- app/Models/InventoryItem.php
+++ app/Models/InventoryItem.php
@@ -4,8 +4,38 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InventoryItem extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    protected static function booted()
+    {
+        static::creating(function (InventoryItem $inventoryItem) {
+            do {
+                // random ref
+                $ref = 'INVIT' . substr(
+                        str_shuffle('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'),
+                        0, 8);
+
+            } while(InventoryItem::where('reference', $ref)->exists());
+
+            $inventoryItem->reference = $ref;
+        });
+    }
+
+    public function inventory(): BelongsTo
+    {
+        return $this->belongsTo(Inventory::class);
+    }
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class);
+    }
 }
